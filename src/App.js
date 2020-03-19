911ab2cc9c702efe0d5a425238fd18bb22d23850
Mayck Xavier <mayckxavier@gmail.com>
2020-03-19 16:26:33 -0300

    Making API request

--- src/App.js
+++ src/App.js
@@ -1,86 +1,48 @@
 import React from "react";
-import Device from './components/Device/Device'
+import Swagger from "swagger-client";
 
-const deviceList = [
-  {
-    deviceId: 19401,
-    deviceType: "Employee Servers",
-    hostname: "",
-    ipAddresses: [],
-    ipmiEnabled: false,
-    location: {
-      cage_id: 28,
-      facility: "LAX2",
-      position: "",
-      qr_code: "",
-      rack: "Rack 27",
-      row_name: "Cage 2"
-    },
-    name: "Device 3",
-    powerStatus: "ON",
-    selfProvisioning: null,
-    serviceMonitors: [],
-    servicePlan: null,
-    status: "active",
-    tags: []
-  },
-  {
-    deviceId: 19400,
-    deviceType: "Employee Servers",
-    hostname: "",
-    ipAddresses: [],
-    ipmiEnabled: false,
-    location: {
-      cage_id: 44,
-      facility: "DAL1",
-      position: "",
-      qr_code: "",
-      rack: "Rack 01",
-      row_name: "Row1"
-    },
-    name: "Device 2",
-    powerStatus: "ON",
-    selfProvisioning: null,
-    serviceMonitors: [],
-    servicePlan: null,
-    status: "active",
-    tags: []
-  },
-  {
-    deviceId: 19399,
-    deviceType: "Employee Servers",
-    hostname: "",
-    ipAddresses: [],
-    ipmiEnabled: false,
-    location: {
-      cage_id: 12,
-      facility: "TPA1",
-      position: "",
-      qr_code: "",
-      rack: "Rack 1",
-      row_name: "All Racks"
-    },
-    name: "Device 1",
-    powerStatus: "ON",
-    selfProvisioning: null,
-    serviceMonitors: [],
-    servicePlan: null,
-    status: "active",
-    tags: []
+import Device from "./components/Device/Device";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deviceList: [],
+      hasFetchError: false
+    };
   }
-];
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from Hivelocity</h1>
-      
+  componentDidMount() {
+    Swagger.http.withCredentials = true;
+    Swagger.http({
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+      url: process.env.REACT_APP_API_URL,
+      headers: new Headers({
+        "X-API-KEY": process.env.REACT_APP_X_API_KEY,
+      })
+    })
+      .then(res => {
+        this.setState({ deviceList: res.body });
+      })
+      .catch(err => {
+        this.setState({ hasFetchError: true });
+        console.log("err", JSON.stringify(err));
+      });
+  }
+  render() {
+    const { deviceList,hasFetchError } = this.state;
+    return (
+      <div className="App">
+        <h1>Hivelocity - Device Status</h1>
         {deviceList.map(device => {
           return <Device {...device} />;
         })}
-      
-    </div>
-  );
+        {hasFetchError && (
+          <h2>Error</h2> 
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
