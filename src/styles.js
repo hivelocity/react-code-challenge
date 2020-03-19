73f2d7624da74bcbfd46cf72a95f32e48ee771f8
Mayck Xavier <mayckxavier@gmail.com>
2020-03-19 17:03:36 -0300

    Creating Title and ErrorMessage components

---
+++ src/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const ErrorMessage = styled.div`
+  background-color: #F8D7DA;
+  border-color:#f5c6cb;
+  color: #721C24;
+  padding: .75em;
+  border-radius: .25em;
+  width: 50%;
+  margin:0 auto;
+`;
