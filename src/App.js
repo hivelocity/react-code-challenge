3ab766ca91c9314603d290f28444b9c513e3598e
Mayck Xavier <mayckxavier@gmail.com>
2020-03-19 13:35:25 -0300

    Mocking API response and creating first list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,92 @@
 import React from 'react';
 
+
+const deviceList = [
+  {
+      "deviceId": 19401,
+      "deviceType": "Employee Servers",
+      "hostname": "",
+      "ipAddresses": [],
+      "ipmiEnabled": false,
+      "location": {
+          "cage_id": 28,
+          "facility": "LAX2",
+          "position": "",
+          "qr_code": "",
+          "rack": "Rack 27",
+          "row_name": "Cage 2"
+      },
+      "name": "Device 3",
+      "powerStatus": "ON",
+      "selfProvisioning": null,
+      "serviceMonitors": [],
+      "servicePlan": null,
+      "status": "active",
+      "tags": []
+  },
+  {
+      "deviceId": 19400,
+      "deviceType": "Employee Servers",
+      "hostname": "",
+      "ipAddresses": [],
+      "ipmiEnabled": false,
+      "location": {
+          "cage_id": 44,
+          "facility": "DAL1",
+          "position": "",
+          "qr_code": "",
+          "rack": "Rack 01",
+          "row_name": "Row1"
+      },
+      "name": "Device 2",
+      "powerStatus": "ON",
+      "selfProvisioning": null,
+      "serviceMonitors": [],
+      "servicePlan": null,
+      "status": "active",
+      "tags": []
+  },
+  {
+      "deviceId": 19399,
+      "deviceType": "Employee Servers",
+      "hostname": "",
+      "ipAddresses": [],
+      "ipmiEnabled": false,
+      "location": {
+          "cage_id": 12,
+          "facility": "TPA1",
+          "position": "",
+          "qr_code": "",
+          "rack": "Rack 1",
+          "row_name": "All Racks"
+      },
+      "name": "Device 1",
+      "powerStatus": "ON",
+      "selfProvisioning": null,
+      "serviceMonitors": [],
+      "servicePlan": null,
+      "status": "active",
+      "tags": []
+  }
+]
+
+
 function App() {
   return (
     <div className="App">
-        <p>Hello from Hivelocity</p>
+    <h1>Hello from Hivelocity</h1>
+    <ul>
+    {
+      deviceList.map(device => {
+        return(<li>{device.name} - {device.powerStatus}</li>);
+      })
+    }
+    </ul>
+
+    
+
+
+        
     </div>
   );
 }
